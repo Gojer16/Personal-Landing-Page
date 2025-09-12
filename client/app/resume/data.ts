bba51b30b4903d6e17c065f1e40e3b383519fd6b
Gojer <gojer@naver.com>
2025-09-12 12:39:28 -0400

    data for resume

---
+++ client/app/resume/data.ts
@@ -0,0 +1,39 @@
+
+export const skills = {
+    Languages: ["JavaScript", "TypeScript", "Python", "HTML", "CSS", "SQL"],
+    Frameworks: ["React", "Next.js", "TailwindCSS", "Framer Motion", "Zustand", "Node.js", "Express.js", "FastAPI", "Flask"],
+    Databases: ["MongoDB", "MySQL", "PostgreSQL", "SQLite"],
+    Tools: ["Docker", "AWS", "Git", "CI/CD", "Github (Version Control)", "Vercel / Render / Heroku", "Github Actions"],
+    Other: ["APIs & REST Design", "Unit & Integration Testing (Jest)", "OAuth2", "Encryption", "Agile / Scrum", "AI Agents"],
+    Learning: ["Nest.js", "GraphQL", "Microservices Architecture", "MCPs"]
+  };
+
+export const projects = [
+    {
+      title: "Room Planner Prototype",
+      description:
+        "Ikea Kreativ-inspired 3D room design prototype built with Next.js + React Three Fiber.",
+      stack: ["Next.js", "Three.js", "React Three Fiber", "TailwindCSS"],
+      link: "/projects/ikea",
+    },
+    {
+      title: "Elevare (Productivity App)",
+      description:
+        "Elevare is a modern productivity app designed to help you organize tasks, reflect on progress, and build better daily habits.",
+      stack: ["Next.js", "TailwindCSS", "Framer Motion", "Prisma", "PostgreSQL", "Zustand", "TypeScript"],
+      link: "/projects/elevare",
+    },
+    {
+      title: "Librora (Book Management App)",
+      description:
+        "A SaaS-style book management platform that combines a global catalog with personal libraries and reading analytics.",
+      stack: ["Next.js", "TailwindCSS", "Framer Motion", "Express.js", "Joi", "MongoDB", "Cloudinary", "TypeScript"],
+      link: "/projects/librora",
+    }, 
+    {
+        title: "Online Resume Website",
+        description: "Responsive portfolio with full-page scroll, animations, and an AI chatbot guide.",
+        stack: ["React", "Framer Motion", "TailwindCSS", "Flask", "Gemini API", "AI Chatbot"],
+        link: "/projects/resumewebsite"
+     }
+  ];
